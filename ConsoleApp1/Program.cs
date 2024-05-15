da8c7ba2fe2c2250cebe9f85f5d79a29f48a84a5
Siddhant09 <siddhant.c09@gmail.com>
2024-05-15 04:30:06 -0400

    changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,22 +8,6 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(ConvertToBinary(26));
 
 Console.ReadLine();
-//*Build a console application to convert from base-ten to base-two
-// using the following algorithm.
-
-//Input value is repeatedly divided by 2, with the remainders
-// giving the binary digits in the base-two representation from
-// left to right.
-
-// For example, the base-two representation of 26 is 11010.
-
-// 26 / 2 = 13, R 0
-// 13 / 2 = 6, R 1
-// 6 / 2 = 3, R 0
-// 3 / 2 = 1, R 1
-// 1 / 2 = 0, R 1
-
-//*/
 
 string ConvertToBinary(int num) // 26
 {
